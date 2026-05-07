3f75ec49cc9e09bc4d99ea44d8a483f9eb2add70
fazlullah <fazlullah6235@gmail.com>
2026-05-07 15:04:06 +0530

    merged kafka branch to main

---
+++ booking-service/src/main/java/com/example/config/KafkaConfig.java
@@ -0,0 +1,27 @@
+package com.example.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic bookingEventsTopic(){
+        return TopicBuilder
+                .name("booking-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+
+    @Bean
+    public NewTopic bookingFinalizedEventsTopic(){
+        return TopicBuilder
+                .name("booking-finalized-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
---
+++ showtime-service/src/main/java/com/example/config/KafkaConfig.java
@@ -0,0 +1,18 @@
+package com.example.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+
+@Configuration
+public class KafkaConfig {
+    @Bean
+    public NewTopic seatEventsTopic(){
+        return TopicBuilder
+                .name("seat-events")
+                .partitions(3)
+                .replicas(1)
+                .build();
+    }
+}
